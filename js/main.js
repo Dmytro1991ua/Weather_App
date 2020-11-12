1003106346a12da983465aaef95c2b36d0627b35
Dmitriy Kurchenko <dmitriy1991dp@gmail.com>
2020-11-12 16:52:08 +0200

    added functionality to render weather app details and catch possible errors update UI / to convert default Kelvin units into Celcius / to change weather app img depends on either it is day or night in a specific city

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 
+"use strict";
 const runScript = () => {
-   "use strict";
 
    // Weather API info
    const weatherAPI = {
@@ -8,21 +8,15 @@ const runScript = () => {
       baseUrl: `http://api.openweathermap.org/data/2.5/weather`,
    }
 
-
    //Global variables
-   const formSearch = document.querySelector(".weather-app__search-input"),
+   const wrapperContent = document.querySelector(".wrapper"),
+      formSearch = document.querySelector(".weather-app__search-input"),
       searchInput = document.querySelector(".weather-app__city-search"),
-      searchBtn = document.querySelector(".weather-app__search-input-btn"),
-      weatherDetailsBody = document.querySelector(".weather-app__bottom"),
-      cityName = document.querySelector(".heading-2");
-      /*temperature = document.querySelector("."),
-      weatherCondition = document.querySelector("."),
-      temperatureMax = document.querySelector("."),
-      temperatureMin = document.querySelector("."),
-      weatherIcon = document.querySelector("."),
-      TemperatureFeeling = document.querySelector(".temp-feeling"),
-      humidity = document.querySelector(".humidity"),*/
-
+      date = document.querySelector(".weather-app__date"),
+      time = document.querySelector(".weather-app__time"),
+      weatherDetails = document.querySelector(".weather-app__bottom"),
+      weatherAppHeader = document.querySelector(".weather-app__header"),
+      weatherAppImg = document.querySelector(".weather-app__img");
 
    // add zero to munites and seconds when needed
    const addZero = (num) => {
@@ -31,7 +25,6 @@ const runScript = () => {
 
    // display curent date as a string on the screen
    const displayCurrentDate = () => {
-      const date = document.querySelector(".weather-app__date");
       const currentDay = new Date();
 
       const options = {
@@ -51,7 +44,6 @@ const runScript = () => {
          minute = today.getMinutes(),
          second = today.getSeconds();
 
-      const time = document.querySelector(".weather-app__time");
       const showAmPm = true;
 
       const amPm = hour >= 12 ? "PM" : "AM"; // figure out either PM or AM
@@ -66,15 +58,14 @@ const runScript = () => {
    // change background image based on current time 
    const changeBgImage = () => {
       const today = new Date(),
-         hour = today.getHours(),
-         wrapper = document.querySelector(".wrapper");
+         hour = today.getHours();
 
       if (hour < 12) {
-         wrapper.style.backgroundImage = 'linear-gradient(to bottom, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.6)), url(../images/bg-covers/sunrise.jpg)';
+         wrapperContent.style.backgroundImage = 'linear-gradient(to bottom, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.6)), url(../images/bg-covers/sunrise.jpg)';
       } else if (hour < 18) {
-         wrapper.style.backgroundImage = 'linear-gradient(to bottom, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.6)), url(../images/bg-covers/sunset.jpg)';
+         wrapperContent.style.backgroundImage = 'linear-gradient(to bottom, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.6)), url(../images/bg-covers/sunset.jpg)';
       } else {
-         wrapper.style.backgroundImage = 'linear-gradient(to bottom, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.6)), url(../images/bg-covers/night.jpg)';
+         wrapperContent.style.backgroundImage = 'linear-gradient(to bottom, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.6)), url(../images/bg-covers/night.jpg)';
       }
    };
 
@@ -83,7 +74,7 @@ const runScript = () => {
       return fetch(url)
          .then(response => {
             if (!response.ok) {
-               throw new Error(`${errorMessage} ${response.status}`)
+               throw new Error(`${errorMessage}:${response.status}`)
             }
             return response.json();
          })
@@ -93,12 +84,92 @@ const runScript = () => {
    const requestCity = (city) => {
       getJSONData(`${weatherAPI.baseUrl}?q=${city}&appid=${weatherAPI.apiKey}`, 'City Not Found')
          .then(data => {
-            console.log(data);
             renderWeatherData(data);
          })
          .catch(error => {
-            console.log(`${error.message}`);
+            renderError(`Something went wrong with your request: ${error.message}, Please Try Again!`);
          });
+   };
+
+
+   // convert default Kelvin units into Celsius
+   const kelvinToCelsius = (kelvin) => {
+      const celcius = Math.round(kelvin - 273.15);
+      return celcius;
+   }
+
+   //check wether if it is currently day or night at the specific city and based on that, change an weather app image
+   const isDayTime = (icon) => {
+      if (icon.includes("d")) {
+         return true;
+      } else {
+         return false;
+      }
+   }
+
+   // render weather data from api and update UI
+   const renderWeatherData = (city) => {
+      const iconPath = city.weather[0].icon; // garb a path to a weather icon from API
+      const weatherIconSrc = `http://openweathermap.org/img/wn/${iconPath}@2x.png`; // change a weather icon depends on a current weather state and either it is day or night  in particular city
+
+      weatherDetails.innerHTML = `
+         <div class="weather-app__bottom-body">
+               <h2 class="heading-2">${city.name},<span class="heading-2--diff">${city.sys.country}</span></h2>
+               <!--First row starts-->
+               <div class="weather-app__row">
+                    <span class="weather-app__temperature">${kelvinToCelsius(city.main.temp)}&deg;C</span>
+                    <div class="weather-app__temperature-details">
+                        <span class="weather-app__weather-condition">${city.weather[0].description}</span>
+                        <span class="weather-app__temperature-max">${kelvinToCelsius(city.main.temp_max)}&deg;C</span>
+                        <span class="weather-app__temperature-min">${kelvinToCelsius(city.main.temp_min)}&deg;C</span>
+                    </div>
+               </div>
+               <!--First row ends-->
+               <!--Second row starts-->
+               <div class="weather-app__row">
+                    <figure class="weather-app__icon-box">
+                        <img src="${weatherIconSrc}" alt="weather icon" class="weather-app__icon">
+                    </figure>
+               </div>
+               <!--Second row ends-->
+               <!--Third row starts-->
+               <div class="weather-app__row">
+                    <div class="weather-app__temperature-additional">
+                        <div class="weather-app__temperature-additional-box">
+                           <span class="weather-app__temperature-feeling">${kelvinToCelsius(city.main.feels_like)}&deg;C</span>
+                           <p class="weather-app__temperature-additional-title">Feels like</p>
+                        </div>
+                           <div class="weather-app__temperature-additional-box">
+                           <span class="weather-app__temperature-feeling">${city.main.humidity}%</span>
+                           <p class="weather-app__temperature-additional-title">Humidity</p>
+                        </div>
+                    </div>
+               </div>
+               <!--Third row ends-->
+         </div> 
+      `
+
+      if (isDayTime(weatherIconSrc)) { // pass recieved from API icon (whether day or night) 
+         weatherAppImg.setAttribute("src", "/images/app-img/day.jpg");
+         weatherDetails.style.backgroundColor = 'var(--color-lighter-blue)';
+         weatherDetails.style.color = 'var(--color-white)';
+         weatherAppHeader.style.color = "var(--color-mantis-darker)";
+
+      } else {
+         weatherAppImg.setAttribute("src", "/images/app-img/night.jpg");
+         weatherDetails.style.backgroundColor = 'var(--color-dark-blue)';
+         weatherDetails.style.color = 'var(--color-mantis)';
+         weatherAppHeader.style.color = 'var(--color-mantis-darker)';
+      }
+
+   };
+
+   // render am Error message text and insert it to HTML
+   const renderError = (message) => {
+      if (message) {
+         weatherDetails.innerHTML = ""; // clear wethear app body to display an error
+         weatherDetails.insertAdjacentHTML("beforeend", message);
+     }
    };
 
 
@@ -108,25 +179,9 @@ const runScript = () => {
 
       const searchedCity = searchInput.value.toLowerCase().trim();
       formSearch.reset();
-
-      requestCity(searchedCity)
-
-      /*if (event.code === "Enter") {
-         event.preventDefault();
-         const searchInput = formSearch.children[0].children[0]; // grab search input
-         const inputValue = searchInput.value;
-         
-         if (inputValue) {
-            console.log("there is a value");
-         } else {
-            console.log("Nope..no value at all");
-         }
-      } */
-
-   });
-
-   searchBtn.addEventListener("click", () => {
-
+      weatherDetails.innerHTML = ""; // clear weather app details card before searching a new city;
+      weatherDetails.classList.add("active");
+      requestCity(searchedCity);
    });
 
    //call functions
